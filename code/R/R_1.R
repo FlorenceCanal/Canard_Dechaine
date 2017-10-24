fd2b85112495bc063bc37f45e316f8e317655d51
FlorenceCanal <florencecanal1@gmail.com>
2017-10-24 10:02:17 +0200

    Update concat_train.py

--- code/R/R_1.R
+++ code/R/R_1.R
@@ -7,6 +7,8 @@ library(corrplot)
 library(mice)
 library(lattice)
 library(VIM)
+library(car)
+library(leaps)
 
 #----------------------------------------
 
@@ -22,7 +24,7 @@ library(VIM)
 #----------
 
 # setwd("~/M2/Apprentissage/Projet/GIT")
-df <- read.csv("./Sakhir/data/final_train.csv.csv", sep=";", dec = ",")
+df <- read.csv("./Sakhir/code/final_train.csv", sep=";", dec = ".")
 
 str(df)
 
@@ -73,8 +75,9 @@ X <- df[,!(colnames(df) %in% c("date", "insee", "ech", "mois"))]
 
 # md.pattern(df)
 aggr_plot <- aggr(X, col=c('navyblue','red'), numbers=T, sortVars=T, labels=names(X), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
-
-
+summary(aggr_plot)
+aggr_plot$percent
+aggr_plot$x
 
 
 
@@ -137,9 +140,6 @@ corrplot(corr, order = "hclust", hclust.method = "ward.D2", diag = F, type="uppe
 
 
 
-
-
-
 #----------
 # 1st submission -> predictions without any correction :
 #----------
@@ -152,6 +152,78 @@ df$tH2_obs <- test$tH2
 # write.csv2(df, file = "./Sakhir/submission/sub_1.csv", quote = F, row.names=F)
 
 head(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----------
+# 2nd submission -> predictions from linear regression model :
+#----------
+
+test <- sample(nrow(df), size = round(nrow(df)*0.3), replace = F)
+learn <- df[-test,]
+test <- df[test,]
+
+df <- learn
+
+df$tH2[is.na(df$tH2)] <- mean(df$tH2, na.rm = T)
+delta <- df$tH2_obs-df$tH2
+X <- as.matrix(cbind(df[,!(colnames(df) %in% c("date", "insee", "tH2", "tH2_obs", "ech", "mois", "flvis1SOL0"))], delta))
+X <- scale(X, center = T, scale = T)
+corr <- cor(cbind(X), method = "pearson", use = "complete.obs")
+corrplot(corr, order = "hclust", hclust.method = "ward.D2", diag = F, type="upper")
+
+
+dfmod <- as.data.frame(cbind(delta, df$insee, df$ciwcH20, df$clwcH20, df$ddH10_rose4, df$ffH10, df$flir1SOL0, df$fllat1SOL0, df$flsen1SOL0, df$hcoulimSOL0, df$huH2, df$iwcSOL0, df$nbSOL0_HMoy, df$nH20, df$ntSOL0_HMoy, df$pMER0, df$rr1SOL0, df$rrH20, df$tH2, df$tH2_VGrad_2.100, df$tH2_XGrad, df$tH2_YGrad, df$tpwHPA850, df$ux1H10, df$vapcSOL0, df$vx1H10, df$ech))
+colnames(dfmod) <- c("delta", "insee", "ciwcH20", "clwcH20", "ddH10_rose4", "ffH10", "flir1SOL0", "fllat1SOL0", "flsen1SOL0", "hcoulimSOL0", "huH2", "iwcSOL0", "nbSOL0_HMoy", "nH20", "ntSOL0_HMoy", "pMER0", "rr1SOL0", "rrH20", "tH2", "tH2_VGrad_2.100", "tH2_XGrad", "tH2_YGrad", "tpwHPA850", "ux1H10", "vapcSOL0", "vx1H10", "ech")
+
+stepwise <- regsubsets(delta ~ . , data = dfmod, method = "seqrep", nbest=1) # method = "forward" / "backward"
+par(mfrow=c(1,2))
+plot(stepwise, scale = "adjr2", main = "Stepwise Selection\nAIC")
+plot(stepwise, scale = "bic", main = "Stepwise Selection\nBIC")
+par(mfrow=c(1,1))
+
+reg <- lm(delta ~ ., data = dfmod)
+summary(reg)
+
+reg <- lm(delta ~ clwcH20 + ddH10_rose4 + ffH10 + fllat1SOL0 + flsen1SOL0 + hcoulimSOL0 + huH2 +
+            nH20 + pMER0 + tH2 + tH2_VGrad_2.100 + tH2_YGrad + tpwHPA850 + vapcSOL0 + vx1H10 + ech, data = dfmod)
+summary(reg)
+
+dfmod2 <- dfmod[,c("delta", "clwcH20", "ddH10_rose4", "ffH10", "fllat1SOL0", "flsen1SOL0", "hcoulimSOL0", "huH2", "nH20", "pMER0", "tH2", "tH2_VGrad_2.100", "tH2_YGrad", "tpwHPA850", "vapcSOL0", "vx1H10")]#, "ech")]
+
+X <- as.matrix(dfmod2)
+X <- scale(X, center = T, scale = T)
+corr <- cor(X, method = "pearson", use = "complete.obs")
+corrplot(corr, order = "hclust", hclust.method = "ward.D2", diag = F, type="upper")
+
+
+
+reg <- lm(delta ~ ddH10_rose4 + fllat1SOL0 + flsen1SOL0 + hcoulimSOL0 + huH2 +
+            nH20 + pMER0 + tH2 + tH2_VGrad_2.100 + tH2_YGrad + vapcSOL0 + vx1H10 + as.factor(ech) + as.factor(insee), data = dfmod)
+summary(reg)
+
+
+
+
+VALID <- read.csv("./Sakhir/data/test/test.csv", sep=";")
+
+
+
+
+
 
 
 
